7eb85436700e119bd06486247d7c05056b62cbe2
nazareno1010 <nazarenobeauvais@gmail.com>
2024-09-05 16:26:48 -0300

    Update .gitignore

--- src/mqtt.cpp
+++
@@ -1,8 +0,0 @@
-#include "mqtt.h"
-
-// MQTT Broker Settings
-const char *mqtt_broker = "airbits.me";
-const char *mqtt_topic = "nubiant/gw/ac233fc17a3a/json/status";
-const char *mqtt_username = "";
-const char *mqtt_password = "";
-const int mqtt_port = 31883;
